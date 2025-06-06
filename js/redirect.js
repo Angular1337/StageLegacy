291251aae903b2061befb52c4a8d467a0c8ee353
Angular1337 <gtmrvsmcpd@gmail.com>
2025-06-06 10:48:06 +0300

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,18 +1,18 @@
 document.getElementById('card__1__download').addEventListener('click', function() {
-        window.location.href = './games/need__for__speed__most__wanted.html'; // Замените на нужный URL
+        window.location.href = 'games/need__for__speed__most__wanted.html'; // Замените на нужный URL
     });
 document.getElementById('card__2__download').addEventListener('click', function() {
-        window.location.href = './games/grand__theft__auto__san__andreas.html'; // Замените на нужный URL
+        window.location.href = 'games/grand__theft__auto__san__andreas.html'; // Замените на нужный URL
     });
 document.getElementById('card__3__download').addEventListener('click', function() {
-        window.location.href = './games/grand__theft__auto__vice__city.html'; // Замените на нужный URL
+        window.location.href = 'games/grand__theft__auto__vice__city.html'; // Замените на нужный URL
     });
 document.getElementById('card__4__download').addEventListener('click', function() {
-        window.location.href = './games/need__for__speed__underground__2.html'; // Замените на нужный URL
+        window.location.href = 'games/need__for__speed__underground__2.html'; // Замените на нужный URL
     });
 document.getElementById('card__5__download').addEventListener('click', function() {
-        window.location.href = './games/bully.html'; // Замените на нужный URL
+        window.location.href = 'games/bully.html'; // Замените на нужный URL
     });
 document.getElementById('card__6__download').addEventListener('click', function() {
-        window.location.href = './games/the__witcher.html'; // Замените на нужный URL
+        window.location.href = 'games/the__witcher.html'; // Замените на нужный URL
     });
